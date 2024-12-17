44a307c321f625a82eea43381e1ef29afb82ac07
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2024-12-17 18:09:05 +0000

    Add of simple template to view

--- eshop/urls.py
+++ eshop/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from front import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('admin_volt.urls')),
+    path('admin/', include('admin_volt.urls')),
+    path('', views.home, name='home'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
