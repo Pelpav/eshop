8a4aad069a87d843bce36fccfa52cbae901e9486
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2024-12-20 19:17:18 +0000

    --amend

--- back/admin.py
+++ back/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Image
 
 # admin.site.register(Product)
 @admin.register(Product)
@@ -7,4 +7,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'slug', 'price', 'active']
     list_filter = ['active', ]
     search_fields = ['id', 'name']
-    
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'file', 'product']
+    search_fields = ['id', 'name', 'product__name']
