a12f646505086216e34741d75c94c52575ac43dd
H Kassogué <hamidoukass@gmail.com>
2025-01-15 19:05:17 +0000

    Make of registration feature

--- myauth/views.py
+++ myauth/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-def login(request):
-    return render(request, 'myauth/login.html', {})
+# def login(request):
+#     return render(request, 'myauth/login.html', {})
 
 def register(request):
-    return render(request, 'myauth/register.html', {})
+    if request.user.is_authenticated:
+        return redirect('home')
+        
+    from django.contrib.auth.forms import UserCreationForm
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        user = form.save()
+        from .models import Customer
+        Customer(user=user).save()
+        from django.contrib.auth import authenticate, login
+        login(request, 
+            authenticate(username=user.username, password=form.cleaned_data['password1']))
+        return redirect('home')
+    return render(request, 'myauth/register.html', {'form': form})
